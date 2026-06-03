eb8bdbc58d238bdd33bb78e57388fee163108fcd
Mayank Rawat <rawatmayankofc25@gmail.com>
2026-06-03 18:29:26 +0530

    Add Home page component with its CSS module
    add home page component with it's CSS module and images to make it better looking for user perspective

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,49 @@
+import styles from "./Home.module.css";
+import Header from "/src/components/header/Header.jsx"
+import shopImage from "/src/assets/shopImage.webp"; // transparent image
+
+
+function Home() {
+    return (
+        <>
+            <Header />
+            <section className={styles.hero}>
+                <div className={styles.heroContent}>
+                    <span className={styles.heroTag}>
+                        GOOD FINDS. GREAT TIMES.
+                    </span>
+
+                    <h1>
+                        Shop the best.
+                        <br />
+                        Live your <span>style.</span>
+                    </h1>
+
+                    <p>
+                        Discover quality products you'll love,
+                        <br />
+                        at prices that make sense.
+                    </p>
+
+                    <button className={styles.ctaBtn}>
+                        Explore Now →
+                    </button>
+
+                    <div className={styles.features}>
+                        <div>🏆 Top Quality</div>
+                        <div>🚚 Fast Delivery</div>
+                        <div>🛡️ Secure Payments</div>
+                        <div>🎧 24/7 Support</div>
+                    </div>
+                </div>
+
+                <div className={styles.heroImage}>
+                    <img src={shopImage} alt="Shopping Products" />
+                </div>
+            </section>
+        </>
+
+    );
+}
+
+export default Home;
