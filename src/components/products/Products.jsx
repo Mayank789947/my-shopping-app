78bc91353a66324a5ba1f7d5091ab7babc2b4469
Mayank Rawat <rawatmayankofc25@gmail.com>
2026-06-04 18:41:47 +0530

    Add Products component
    add products component that show list of products fetched from the API

---
+++ src/components/products/Products.jsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react"
+import styles from "./Products.module.css"
+import Card from "../card/Card"
+import { useNavigate } from "react-router-dom"
+import Loading from "../loading/Loading"
+import ErrorPage from "../error/ErrorPage"
+
+function Products() {
+    const [data, setData] = useState(null)
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch("https://fakestoreapi.com/products")
+
+                if (!response.ok) {
+                    throw new Error(`Error: ${response.status}`)
+                }
+
+                const data = await response.json()
+                setData(data)
+                setError(null)
+            } catch (error) {
+                setError(error)
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        fetchData()
+    }, [])
+
+    if (loading) return <Loading />
+    if (error) {
+        return (
+            <ErrorPage
+                title="Unable to load products"
+                message={error.message}
+            />
+        )
+    }
+
+    function handleClick(id) {
+        navigate(`/products/${id}`)
+    }
+
+    return (
+        <>
+            <div className={styles.productsGrid}>
+                {data && data.map((product) => (
+                    <Card
+                        key={product.id}
+                        product={product}
+                        onClick={() => handleClick(product.id)}
+                    />
+                ))}
+            </div>
+        </>
+    )
+
+}
+
+export default Products
