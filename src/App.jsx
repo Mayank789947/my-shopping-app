f15f97e6b4e69ba2aafa42e70ab6ccd9a97dca43
Mayank Rawat <rawatmayankofc25@gmail.com>
2026-06-04 18:52:52 +0530

    Add basic routing in App
    add basic routing that allow user to navigate when user click on any product to single product page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
+import Card from './components/card/Card'
+import Home from './components/home/Home'
+import ProductDetailsPage from './components/productDetailsPage/ProductDetailsPage'
+import Products from './components/products/Products'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "products",
+    element: <Products />,
+  }, 
+  {
+    path: "products/:id",
+    element: <ProductDetailsPage />
+  },
+])
 
 function App() {
 
   return (
     <>
+     <RouterProvider router={router} />
     </>
+     
   )
 }
 
