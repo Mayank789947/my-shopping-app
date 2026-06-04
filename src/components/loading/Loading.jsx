df90e626476ef68fce750ea37674bc2a3ca3aa21
Mayank Rawat <rawatmayankofc25@gmail.com>
2026-06-04 18:31:46 +0530

    Add Loading Spinner component
    add loading spinner component so that we can use it throughout the app without duplicating it everywhere

---
+++ src/components/loading/Loading.jsx
@@ -0,0 +1,13 @@
+import styles from "/src/components/loading/Loading.module.css"
+
+function Loading() {
+  return (
+    <div className={styles.loadingContainer}>
+      <div className={styles.spinner}>
+      </div>
+      <p>Loading...</p>
+    </div>
+  );
+}
+
+export default Loading
