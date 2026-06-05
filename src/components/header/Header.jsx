57180ebb3f7e4a9be640d8161b1985418630e8e1
Mayank Rawat <rawatmayankofc25@gmail.com>
2026-06-05 19:29:57 +0530

    Add navigation on header for different pages
    add navigation on header to navigate through different pages of the app

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,37 @@
+import { useNavigate } from 'react-router-dom'
 import styles from './Header.module.css'
+import { useContext } from 'react'
+import { CartContext } from '../../context/CartContext'
 
 function Header () {
+
+    const navigate = useNavigate()
+    const { cart } = useContext(CartContext)
+
+    function handleHome() {
+      navigate(`/`)
+    }
+
+    function handleProducts() {
+       navigate(`/products`)
+    }
+
+    function handleCart() {
+        navigate(`/cartpage`)
+    }
+
     return (
         <>
          <div className={styles.header}>
             <div className={styles.logo}>Shoppers</div>
             <ul className={styles.listContainer}>
-                <li>Home</li>
-                <li>Products</li>
+                <li onClick={handleHome}>Home</li>
+                <li onClick={handleProducts}>Products</li>
                 <li>About</li>
             </ul>
             <div className={styles.orderContainer}>
                 <button className={styles.btn}>Order now</button>
-                <i>cart image</i>
+                <div onClick={handleCart}>🛒{cart.length > 0 && (cart.length)}</div>
             </div>
          </div>
         </>
